af4a499e13841f7600dee5c8e0fc21076a064f71
Ren Roetzel <renroetzel@gmail.com>
2023-06-22 16:04:42 -0400

    first commit, functional without tests

---
+++ forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+class UserUpdateForm(FlaskForm):
+    """Form for updating users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    header_image_url = StringField('(Optional) Image URL')
+    image_url = StringField('(Optional) Image URL')
+    bio = TextAreaField('(Optional) Bio')
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
